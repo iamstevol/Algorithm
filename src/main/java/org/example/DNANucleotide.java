6fbab22a9932fff2fa313719a3e70b40487903ca
iamstevol <stevol2015@gmail.com>
2023-01-25 08:44:04 +0100

    New commit of algos

--- src/main/java/org/example/DNANucleotide.java
+++ src/main/java/org/example/DNANucleotide.java
@@ -4,16 +4,25 @@ public class DNANucleotide {
 
     public static void main(String[] args) {
 
-        String dna = "G";
+        String dna = "GAT";
         System.out.println(nuc(dna));
     }
 
     public static String nuc(String dna) {
 
-        String result = "";
+        String result ="";
+        if(dna.equals("")) {
+            return "";
+        }
         for (int i = 0; i <dna.length() - 1; i++) {
             if(dna.charAt(i) == 'A') {
                 result += "T";
+            } else if(dna.charAt(i) == 'T') {
+                result += "A";
+            } else if(dna.charAt(i) == 'C') {
+                result += "G";
+            } else if(dna.charAt(i) == 'G') {
+                result += "C";
             }
         }
         return result;
